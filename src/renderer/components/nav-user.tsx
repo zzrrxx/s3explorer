bc1c285c44711c879b6e7f863aea649373758797
martinzrx <253951598@qq.com>
2025-02-17 17:59:42 +0800

    Add sidebar.
    Shadcn ui - "A sidebar that collapses to icons"

--- src/renderer/components/nav-user.tsx
+++ src/renderer/components/nav-user.tsx
@@ -44,7 +44,7 @@ export default function NavUser({
           <DropdownMenuTrigger asChild>
             <SidebarMenuButton
               size="lg"
-              className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground md:h-8 md:p-0"
+              className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
             >
               <Avatar className="h-8 w-8 rounded-lg">
                 <AvatarImage src={user.avatar} alt={user.name} />
